ee9756b7895dc3c2875f7e63702121b6aa6a77d2
Gonzalo Aranda <c_gonzara@convera.com>
2025-04-01 00:26:48 -0300

    - MessageUtilTest refactor

--- src/test/java/com/core/elevator_api/util/MessageUtilTest.java
+++ src/test/java/com/core/elevator_api/util/MessageUtilTest.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
-class MesageUtilTest {
+class MessageUtilTest {
 
   @Test
   void createElevatorWeightExceededMessageTest() {
